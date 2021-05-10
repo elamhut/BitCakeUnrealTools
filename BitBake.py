28e8e76c66286e02f1a585a4c180bc2228d8a27a
elamhut <eduardo@bitcake.studio>
2021-05-10 01:50:58 -0300

    Renomeando pasta

--- BitBake.py
+++ BitBake.py
@@ -2,6 +2,7 @@ import unreal
 import subprocess
 import os
 import datetime
+from shutil import rmtree
 
 project_name = "NekoNeko"
 project_dir = unreal.Paths().project_dir()
@@ -13,6 +14,7 @@ engine_location = unreal.Paths().engine_dir()
 engine_location = unreal.Paths().convert_relative_path_to_full(engine_location)
 editor_location = "{}Binaries/Win64/UE4Editor.exe".format(engine_location)
 editor_cmd_location = "{}Binaries/Win64/UE4Editor-cmd.exe".format(engine_location)
+build_location = "{}Build/".format(project_dir)
 batch_files_dir = "{}Build/BatchFiles/".format(engine_location)
 
 
@@ -26,7 +28,7 @@ def build():
                             '-project="{}"'.format(uproject_path),
                             '-cook',
                             '-stage',
-                            '-archivedirectory="{}Build"'.format(project_dir),
+                            '-archivedirectory="{}"'.format(build_location),
                             '-package',
                             '-pak',
                             '-prereqs',
@@ -40,9 +42,19 @@ def build():
 
     if build.returncode == 0:
         date = datetime.datetime.now().strftime("%y%m%d")
-        folder_name = "{}_NekoNeko".format(date)
-        os.rename("{}Build/WindowsNoEditor/".format(project_dir), "{}Build/{}/".format(project_dir, folder_name))
+        folder_name = "{}_{}".format(date, project_name)
+        constructed_path = "{}{}".format(build_location, folder_name)
+        if os.path.isdir("{}".format(constructed_path)):
+            rmtree("{}".format(constructed_path))
+            os.rename("{}WindowsNoEditor/".format(build_location), "{}".format(constructed_path))
+        else:
+            os.rename("{}WindowsNoEditor/".format(build_location), "{}".format(constructed_path))
+
 
 
 if __name__ == "__main__":
     build()
+    # date = datetime.datetime.now().strftime("%y%m%d")
+    # folder_name = "{}_{}".format(date, project_name)
+    # constructed_path = "{}{}".format(build_location, folder_name)
+    # print(constructed_path)
--- Tests.py
+++ Tests.py
@@ -1,3 +1,8 @@
 import datetime
+import os
 
+path = os.getcwd()
+print("{}\\AgoraVai".format(path))
+exist = os.path.isdir("{}\\AgoraVai\\".format(path))
+print(exist)
 print(datetime.datetime.now().strftime("%y%m%d"))
